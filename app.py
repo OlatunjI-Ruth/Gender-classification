3bfefe58da5783ad97a230ac03501c3cc25dbffa
OlatunjI-Ruth <85808869+OlatunjI-Ruth@users.noreply.github.com>
2022-07-24 18:44:42 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, send_file
+from PIL import Image
+import io
+import numpy as np
+from keras.models import load_model
+import re
+import pathlib
+import sys
+import os
+from keras.utils import load_img, img_to_array
+from werkzeug.utils import secure_filename
+sys.path.append(os.path.abspath("./model"))
+#from loader import *
+
+#global graph, model
+#model = init()
+
+app = Flask(__name__, template_folder='Template')
+model = load_model('model.h5')
+
+@app.route('/')
+def index_view():
+    return render_template("index.html")
+
+
+@app.route('/submit', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        image = request.files['my_image']
+        image_path = "./media/" + image.filename
+        image.save(image_path)
+
+        img = load_img(image_path, target_size=(150, 150))
+        x = img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        image_tensor = np.vstack([x])
+        classes = model.predict(image_tensor)
+        print(classes[0])
+        if classes[0][0] > 50:
+            response = 'This image is a cat'
+        else:
+            classes[0][1] > 70
+            response = 'This image is a dog'
+    return render_template("index.html", prediction=response, image=image)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
+
+
+
