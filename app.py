a610af97ccb286323b9a6b42f212e5488370a749
OlatunjI-Ruth <85808869+OlatunjI-Ruth@users.noreply.github.com>
2022-07-24 19:00:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,45 +7,44 @@ import re
 import pathlib
 import sys
 import os
+from tensorflow.keras.preprocessing import image
 from keras.utils import load_img, img_to_array
 from werkzeug.utils import secure_filename
 sys.path.append(os.path.abspath("./model"))
-#from loader import *
+global graph, model
 
-#global graph, model
-#model = init()
 
 app = Flask(__name__, template_folder='Template')
-model = load_model('model.h5')
+model = load_model('gendermodel.h5')
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index_view():
     return render_template("index.html")
 
 
-@app.route('/submit', methods=['POST'])
+@app.route('/submit', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
-        image = request.files['my_image']
-        image_path = "./media/" + image.filename
+        image = request.files['file']
+        basepath = os.path.dirname(__file__)
+        image_path = os.path.join(basepath, secure_filename(image.filename))
         image.save(image_path)
-
-        img = load_img(image_path, target_size=(150, 150))
+        img = load_img(image_path, target_size=(300, 300))
         x = img_to_array(img)
+        x=x/225
         x = np.expand_dims(x, axis=0)
         image_tensor = np.vstack([x])
         classes = model.predict(image_tensor)
-        print(classes[0])
-        if classes[0][0] > 50:
-            response = 'This image is a cat'
+        classes = np.argmax(classes, axis=1)
+        #print(classes * 10)
+        if classes==0:
+            print('This is a male gender')
         else:
-            classes[0][1] > 70
-            response = 'This image is a dog'
+            print('This is a female gender')
     return render_template("index.html", prediction=response, image=image)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
-
-
-
+    port = int(os.getenv('PORT', 5000))
+    app.debug = True
+    app.run(host='0.0.0.0', port=port)
